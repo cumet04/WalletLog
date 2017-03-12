4eca2b35e4692abf3e83dd00afb65e4b553da5d1
medalhkr <medalhkr@gmail.com>
2017-03-13 07:43:52 +0900

    [add] add functions of add/list transaction(s) with DB

--- account/account.go
+++ account/account.go
@@ -1,6 +1,13 @@
 package account
 
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
+)
 
 type Transaction struct {
 	Type      string    `json:"type"`
@@ -11,7 +18,86 @@ type Transaction struct {
 	Temporary bool      `json:"temporary"`
 }
 
-var TransList = []Transaction{
-	Transaction{"visa", time.Now(), 2000, "kabe", "20170310, 1000, kabe", true},
-	Transaction{"suica", time.Now(), 100, "zihanki", "20170312, 100, zihanki", true},
+var dbdsn string
+
+const tblTrans = "transactions"
+
+func AddTrans(tr Transaction) error {
+	db, err := sql.Open("mysql", dbdsn)
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to db")
+	}
+	defer db.Close()
+
+	q := fmt.Sprintf("INSERT INTO %s VALUES('%s', '%s', %d, '%s', '%s', %t)", tblTrans,
+		tr.Type, tr.Time.UTC().Format(time.RFC3339), tr.Price, tr.Content, tr.Raw, tr.Temporary)
+	if _, err = db.Exec(q); err != nil {
+		return errors.Wrap(err, "failed to insert query")
+	}
+
+	return nil
+}
+
+func QueryTrans(q string) ([]Transaction, error) {
+	// FIXME: MySQLのコネクションは毎度貼り直すのか残すべきか
+	db, err := sql.Open("mysql", dbdsn)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect to db")
+	}
+	defer db.Close()
+
+	if q == "" {
+		q = "SELECT * FROM " + tblTrans
+	}
+	rows, err := db.Query(q)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to query")
+	}
+	defer rows.Close()
+
+	var list []Transaction
+	for rows.Next() {
+		var tr Transaction
+		err := rows.Scan(&tr.Type, &tr.Time, &tr.Price, &tr.Content, &tr.Raw, &tr.Temporary)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to scan transaction")
+		}
+		tr.Time = tr.Time.Local()
+		list = append(list, tr)
+	}
+	return list, nil
+}
+
+func SetDataSoruceName(dsn string) {
+	dbdsn = dsn
+}
+
+func ClearDB() error {
+	// for unit test
+
+	db, err := sql.Open("mysql", dbdsn)
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to DB")
+	}
+	defer db.Close()
+
+	queries := []string{
+		"DROP TABLE IF EXISTS " + tblTrans,
+		"CREATE TABLE " + tblTrans + "(" +
+			"type      TEXT NOT NULL," +
+			"time      DATETIME NOT NULL," +
+			"price     INT NOT NULL," +
+			"content   TEXT NOT NULL," +
+			"raw       TEXT NOT NULL," +
+			"temporary BOOL NOT NULL" +
+			")",
+	}
+	for _, q := range queries {
+		_, err = db.Exec(q)
+		if err != nil {
+			return errors.Wrap(err, "failed to exec query: "+q)
+		}
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/medalhkr/WalletLog/account"
 )
 
-func apiTransactionsPUT(w http.ResponseWriter, r *http.Request) {
+func apiAddTrans(w http.ResponseWriter, r *http.Request) {
 	var input account.Transaction
 	dec := json.NewDecoder(r.Body)
 	err := dec.Decode(&input)
@@ -16,29 +16,42 @@ func apiTransactionsPUT(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(400)
 		fmt.Fprint(w, "failed to unmarshal input")
 	}
+	if err := account.AddTrans(input); err != nil {
+		w.WriteHeader(500)
+		fmt.Print(err)
+		fmt.Fprint(w, "failed to add transaction")
+	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	fmt.Fprint(w, input)
 }
 
-func apiTransactionsGET(w http.ResponseWriter, r *http.Request) {
-	data, err := json.Marshal(account.TransList)
+func apiQueryTrans(w http.ResponseWriter, r *http.Request) {
+	res, err := account.QueryTrans("")
 	if err != nil {
 		w.WriteHeader(500)
+		fmt.Print(err)
+		fmt.Fprint(w, "failed")
+	}
+	data, err := json.Marshal(res)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Print(err)
 		fmt.Fprint(w, "failed")
 	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Write(data)
 }
 
+func transactionsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		apiQueryTrans(w, r)
+	case "POST":
+		apiAddTrans(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/transactions/",
-		func(w http.ResponseWriter, r *http.Request) {
-			switch r.Method {
-			case "GET":
-				apiTransactionsGET(w, r)
-			case "PUT":
-				apiTransactionsPUT(w, r)
-			}
-		})
+	http.HandleFunc("/transactions/", transactionsHandler)
 	http.ListenAndServe(":50000", nil)
 }
